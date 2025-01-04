4fe1ac542614222c609676fb7cdf8166b957c798
Allison Crenshaw <allisonecrenshaw@gmail.com>
2025-01-04 13:12:48 -0600

    HelloWorld webdriver test from Selenium docs.

---
+++ src/test/java/com/example/sharereadsautomation/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.example.sharereadsautomation;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://selenium.dev");
+
+        driver.quit();
+    }
+}
