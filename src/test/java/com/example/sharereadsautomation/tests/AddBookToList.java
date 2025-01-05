02be4cc038f73cecda35b14ff5f780d60340bcdd
Allison Crenshaw <allisonecrenshaw@gmail.com>
2025-01-04 20:12:54 -0600

    Added validations for AddBookToList test using list url copied from clipboard.

--- src/test/java/com/example/sharereadsautomation/tests/AddBookToList.java
+++ src/test/java/com/example/sharereadsautomation/tests/AddBookToList.java
@@ -2,6 +2,12 @@ package com.example.sharereadsautomation.tests;
 
 import java.time.Duration;
 import java.util.List;
+import java.awt.Toolkit;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.Transferable;
+import java.awt.datatransfer.UnsupportedFlavorException;
+import java.io.IOException;
 
 import org.junit.jupiter.api.Assertions;
 import org.openqa.selenium.By;
@@ -17,13 +23,41 @@ public class AddBookToList {
 
     driver.get("https://sharereads.xyz/");
 
+    // Search for a book
     WebElement bookSearchInput = driver.findElement(By.id("book-search-input"));
     bookSearchInput.sendKeys("ispy");
 
     WebElement bookSearchButton = driver.findElement(By.id("book-search-button"));
     bookSearchButton.click();
   
-    // Add logic here to add book to list and verify those results
+    // Add book to list
+    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(2));
+    WebElement addToListButton = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("book-search-result-add-button")));
+    addToListButton.click();
+
+    WebElement viewListButton = driver.findElement(By.id("view-book-list-button"));
+    viewListButton.click();
+
+    // Copy share link
+    WebElement copyShareLinkButton = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("copy-share-link-button")));
+    copyShareLinkButton.click();
+    
+    // Get value from clipboard
+    Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+    Transferable clipboardContents = clipboard.getContents(clipboard);
+    String bookListUrl = "";
+    try {
+      bookListUrl = (String)clipboardContents.getTransferData(DataFlavor.stringFlavor);
+    } catch (UnsupportedFlavorException | IOException ex) {
+      System.out.println(ex);
+      ex.printStackTrace();
+    }
+
+    // Navigate to list and verify list is not empty
+      // Note: Search results don't have unique ids, so only checking for now that list is not empty
+    driver.get(bookListUrl);
+    List<WebElement> bookListItems = driver.findElements(By.cssSelector("ul > li"));
+    Assertions.assertTrue(bookListItems.size() > 0);
 
     driver.quit();
 
